b5a48b936bb11c4467d98047b32d8075aad325cc
dani herrera <ctrl.alt.d@gmail.com>
2019-11-21 19:55:15 +0100

    Mostrar univers

--- Data/Universe.cs
+++ Data/Universe.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
 
-namespace BlazapyBird.Universe
+namespace BlazapyBird.Data
 {
     public class Universe
     {
+        public string CssStyle => $"width: {SCREENWIDTH}px; height: {SCREENHEIGHT}px; ";
         public const int FPS = 30;
         public const int SCREENWIDTH  = 288;
 
@@ -15,8 +17,42 @@ namespace BlazapyBird.Universe
         //# image, sound and hitmask  dicts
         //IMAGES, SOUNDS, HITMASKS = {}, {}, {}
 
+        public string CurrentBackgroundImage {get; set; } = BACKGROUNDS_LIST[0];
+
+        public static Dictionary<string, string[]> IMAGESS =new Dictionary<string, string[]>() 
+        {
+            ["numbers"] = new [] {
+                "assets/sprites/0.png",
+                "assets/sprites/1.png",
+                "assets/sprites/2.png",
+                "assets/sprites/3.png",
+                "assets/sprites/4.png",
+                "assets/sprites/5.png",
+                "assets/sprites/6.png",
+                "assets/sprites/7.png",
+                "assets/sprites/8.png",
+                "assets/sprites/9.png",
+            },            
+        };
+
+        public static Dictionary<string, string> IMAGES = new Dictionary<string, string>() 
+        {
+            ["gameover"] ="assets/sprites/gameover.png",
+            ["message"] ="assets/sprites/gameover.png",
+            ["base"] ="assets/sprites/gameover.png",
+        };
+
+        public static Dictionary<string, string> SOUNDS = new Dictionary<string, string>() 
+        {
+            ["die"] ="assets/audio/die.ogg",
+            ["hit"] ="assets/audio/hit.ogg",
+            ["point"] ="assets/audio/point.ogg",
+            ["swoosh"] ="assets/audio/swoosh.ogg",
+            ["swoosh"] ="assets/audio/wing.ogg",            
+        };
+
         //list of all possible players (tuple of 3 positions of flap)
-        public IEnumerable<IEnumerable<string>> PLAYERS_LIST = new []
+        public static string[][] PLAYERS_LIST = new []
         {
             // red bird
             new []
@@ -46,14 +82,14 @@ namespace BlazapyBird.Universe
 
 
         // list of backgrounds
-        IEnumerable<string> BACKGROUNDS_LIST = new []
+        public static string[] BACKGROUNDS_LIST = new []
         {
             "assets/sprites/background-day.png",
             "assets/sprites/background-night.png",
         };
 
         // list of pipes
-        IEnumerable<string> PIPES_LIST = new []
+        public static string[] PIPES_LIST = new []
         {
             "assets/sprites/pipe-green.png",
             "assets/sprites/pipe-red.png",
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BlazapyBird.Data;
 
 namespace BlazapyBird
 {
@@ -27,6 +28,7 @@ namespace BlazapyBird
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<Universe>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
